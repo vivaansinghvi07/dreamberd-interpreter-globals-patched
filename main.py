2fd8401240a662ee51bc1f568c9635287ee3212c
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-02-14 20:14:05 -0500

    prevent globals for keywords

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ And does the following:
     1) Create an entry for the data in a file with the name pointing to the respective ID 
     2) Create a file in the `global_objects` directory with the name of the ID and the information in the variable
 """
+import re
 import os 
 import github
 import random
@@ -18,10 +19,14 @@ if __name__ == "__main__":
     NAME_TO_ID_SEP = ";;;"
     VARIABLE_NAME, CONFIDENCE = os.environ["ISSUE_TITLE"].removeprefix("Create Public Global: ").split(NAME_TO_ID_SEP)
     ISSUE_BODY = os.environ["ISSUE_BODY"]
+    KEYWORDS = {'class', 'className', 'after', 'const', 'var', 'when', 'if', 'async', 'return', 'delete', 'await', 'previous', 'next', 'reverse', 'export', 'import'}
 
     value_id = -1
     while value_id < 0 or value_id in os.listdir("global_objects"):
         value_id = random.randint(1, 1000000000)
+
+    if VARIABLE_NAME in KEYWORDS or re.match(r'^f?u?n?c?t?i?o?n?$', VARIABLE_NAME):
+        exit()
 
     with open(f"./global_objects/{value_id}", 'w') as f:
         f.write(ISSUE_BODY)
