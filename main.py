f31e111eeb257580f99d5ea1eb43aa3bcd5792fb
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-03-23 01:12:17 -0400

    moved everything to another folder to avoid vul

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ This script takes an issue containing:
     2) Pickled data in the form of a string of four-digit numbers (representing bytes)
 And does the following:
     1) Create an entry for the data in a file with the name pointing to the respective ID 
-    2) Create a file in the `global_objects` directory with the name of the ID and the information in the variable
+    2) Create a file in the `serialized_objects` directory with the name of the ID and the information in the variable
 """
 import re
 import os 
@@ -25,10 +25,10 @@ if __name__ == "__main__":
         exit()
 
     value_id = -1
-    while value_id < 0 or value_id in os.listdir("global_objects"):
+    while value_id < 0 or value_id in os.listdir("serialized_objects"):
         value_id = random.randint(1, 1000000000)
 
-    with open(f"./global_objects/{value_id}", 'w') as f:
+    with open(f"./serialized_objects/{value_id}", 'w') as f:
         f.write(ISSUE_BODY)
     with open(f"./public_globals.txt", 'a') as f:
         f.write(f"{VARIABLE_NAME}{NAME_TO_ID_SEP}{value_id}{NAME_TO_ID_SEP}{CONFIDENCE}\n")
